670f41dc1979dea3e8ce02a5f451908e63e7ea5f
Darren <2504497@uad.ac.uk>
2025-10-22 13:51:07 +0100

    Added SQL Connection

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +17,31 @@ namespace ScottishGlen
         {
             InitializeComponent();
         }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Server=localhost;Database=myappdb;Uid=root;Pwd=;";
+            string query = "SELECT username FROM users"; // Replace with your table/column
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    listBoxUsers.Items.Clear();
+                    while (reader.Read())
+                    {
+                        listBoxUsers.Items.Add(reader["username"].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
     }
-}
