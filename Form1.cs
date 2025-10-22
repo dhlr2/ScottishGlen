9b969978eaa9d66d3a072a0bd825e95ff5f48db1
Darren <2504497@uad.ac.uk>
2025-10-22 13:55:12 +0100

    Update Form1.cs
    Removed database button test

--- Form1.cs
+++ Form1.cs
@@ -17,31 +17,5 @@ namespace ScottishGlen
         {
             InitializeComponent();
         }
-
-        private void btnLoad_Click(object sender, EventArgs e)
-        {
-            string connectionString = "Server=localhost;Database=myappdb;Uid=root;Pwd=;";
-            string query = "SELECT username FROM users"; // Replace with your table/column
-
-            try
-            {
-                using (MySqlConnection conn = new MySqlConnection(connectionString))
-                {
-                    conn.Open();
-                    MySqlCommand cmd = new MySqlCommand(query, conn);
-                    MySqlDataReader reader = cmd.ExecuteReader();
-
-                    listBoxUsers.Items.Clear();
-                    while (reader.Read())
-                    {
-                        listBoxUsers.Items.Add(reader["username"].ToString());
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error: " + ex.Message);
-            }
-
-        }
     }
+}
