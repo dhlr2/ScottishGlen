986c8e40cf49ebf0fce0ded30aa63cdaa64b245e
Darren <2504497@uad.ac.uk>
2025-10-24 14:41:43 +0100

    Created landing screen
    Created and done a brief design of the landing screen. This will be updated in future

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,28 +48,25 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(343, 67);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(514, 103);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(385, 312);
+            this.panel1.Size = new System.Drawing.Size(578, 480);
             this.panel1.TabIndex = 0;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(128, 182);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Location = new System.Drawing.Point(192, 280);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(143, 35);
+            this.button3.Size = new System.Drawing.Size(214, 54);
             this.button3.TabIndex = 6;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(121, 151);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Location = new System.Drawing.Point(182, 232);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(158, 20);
+            this.textBox2.Size = new System.Drawing.Size(235, 26);
             this.textBox2.TabIndex = 5;
             this.textBox2.UseSystemPasswordChar = true;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
@@ -77,58 +74,52 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(83, 153);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(124, 235);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(51, 20);
             this.label2.TabIndex = 4;
             this.label2.Text = "label2";
             // 
             // loginTxtBox
             // 
-            this.loginTxtBox.Location = new System.Drawing.Point(121, 116);
-            this.loginTxtBox.Margin = new System.Windows.Forms.Padding(2);
+            this.loginTxtBox.Location = new System.Drawing.Point(182, 178);
             this.loginTxtBox.Name = "loginTxtBox";
-            this.loginTxtBox.Size = new System.Drawing.Size(158, 20);
+            this.loginTxtBox.Size = new System.Drawing.Size(235, 26);
             this.loginTxtBox.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(83, 118);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(124, 182);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(51, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(196, 3);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(294, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(187, 55);
+            this.button2.Size = new System.Drawing.Size(280, 85);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(2, 3);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(3, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(196, 55);
+            this.button1.Size = new System.Drawing.Size(294, 85);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1097, 748);
+            this.ClientSize = new System.Drawing.Size(1639, 815);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
---
+++ LandingScreen.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ScottishGlen
+{
+    partial class LandingScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.landingScreenP1 = new System.Windows.Forms.Panel();
+            this.landingScreenP2 = new System.Windows.Forms.Panel();
+            this.loggedAssestsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // landingScreenP1
+            // 
+            this.landingScreenP1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.landingScreenP1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.landingScreenP1.Location = new System.Drawing.Point(0, 0);
+            this.landingScreenP1.Name = "landingScreenP1";
+            this.landingScreenP1.Size = new System.Drawing.Size(337, 866);
+            this.landingScreenP1.TabIndex = 0;
+            // 
+            // landingScreenP2
+            // 
+            this.landingScreenP2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.landingScreenP2.Location = new System.Drawing.Point(336, 0);
+            this.landingScreenP2.Name = "landingScreenP2";
+            this.landingScreenP2.Size = new System.Drawing.Size(934, 104);
+            this.landingScreenP2.TabIndex = 1;
+            // 
+            // loggedAssestsLabel
+            // 
+            this.loggedAssestsLabel.AutoSize = true;
+            this.loggedAssestsLabel.Font = new System.Drawing.Font("MS PGothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loggedAssestsLabel.Location = new System.Drawing.Point(622, 116);
+            this.loggedAssestsLabel.Name = "loggedAssestsLabel";
+            this.loggedAssestsLabel.Size = new System.Drawing.Size(366, 36);
+            this.loggedAssestsLabel.TabIndex = 2;
+            this.loggedAssestsLabel.Text = "Current Logged Assets";
+            // 
+            // LandingScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1269, 866);
+            this.Controls.Add(this.loggedAssestsLabel);
+            this.Controls.Add(this.landingScreenP2);
+            this.Controls.Add(this.landingScreenP1);
+            this.Name = "LandingScreen";
+            this.Text = "LandingScreen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel landingScreenP1;
+        private System.Windows.Forms.Panel landingScreenP2;
+        private System.Windows.Forms.Label loggedAssestsLabel;
+    }
+}
