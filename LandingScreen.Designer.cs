c6f1ab85c676f76d76779dd4af9bb1075dd2f6e1
Darren <2504497@uad.ac.uk>
2025-11-03 23:34:13 +0000

    Updated Landing Screen
    Updates:
    - added a list view
    - cursor changes to hand for usability
    - linked add asset form

--- LandingScreen.Designer.cs
+++ LandingScreen.Designer.cs
@@ -28,57 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.landingScreenP1 = new System.Windows.Forms.Panel();
-            this.landingScreenP2 = new System.Windows.Forms.Panel();
-            this.loggedAssestsLabel = new System.Windows.Forms.Label();
+            this.headingLbl = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.mySystemMenuBtn = new System.Windows.Forms.Button();
+            this.employeeMenuBtn = new System.Windows.Forms.Button();
+            this.assetMenuBtn = new System.Windows.Forms.Button();
+            this.addAssetBtn = new System.Windows.Forms.Button();
+            this.listViewAssets = new System.Windows.Forms.ListView();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // landingScreenP1
+            // headingLbl
             // 
-            this.landingScreenP1.BackColor = System.Drawing.Color.DeepSkyBlue;
-            this.landingScreenP1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.landingScreenP1.Location = new System.Drawing.Point(0, 0);
-            this.landingScreenP1.Name = "landingScreenP1";
-            this.landingScreenP1.Size = new System.Drawing.Size(337, 822);
-            this.landingScreenP1.TabIndex = 0;
+            this.headingLbl.AutoSize = true;
+            this.headingLbl.Font = new System.Drawing.Font("MS PGothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.headingLbl.Location = new System.Drawing.Point(706, 25);
+            this.headingLbl.Name = "headingLbl";
+            this.headingLbl.Size = new System.Drawing.Size(120, 36);
+            this.headingLbl.TabIndex = 2;
+            this.headingLbl.Text = "Assets";
             // 
-            // landingScreenP2
+            // panel2
             // 
-            this.landingScreenP2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.landingScreenP2.Location = new System.Drawing.Point(336, 0);
-            this.landingScreenP2.Name = "landingScreenP2";
-            this.landingScreenP2.Size = new System.Drawing.Size(934, 104);
-            this.landingScreenP2.TabIndex = 1;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(113)))), ((int)(((byte)(207)))));
+            this.panel2.Controls.Add(this.mySystemMenuBtn);
+            this.panel2.Controls.Add(this.employeeMenuBtn);
+            this.panel2.Controls.Add(this.assetMenuBtn);
+            this.panel2.Location = new System.Drawing.Point(-3, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(260, 940);
+            this.panel2.TabIndex = 3;
             // 
-            // loggedAssestsLabel
+            // mySystemMenuBtn
             // 
-            this.loggedAssestsLabel.AutoSize = true;
-            this.loggedAssestsLabel.Font = new System.Drawing.Font("MS PGothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loggedAssestsLabel.Location = new System.Drawing.Point(622, 116);
-            this.loggedAssestsLabel.Name = "loggedAssestsLabel";
-            this.loggedAssestsLabel.Size = new System.Drawing.Size(366, 36);
-            this.loggedAssestsLabel.TabIndex = 2;
-            this.loggedAssestsLabel.Text = "Current Logged Assets";
+            this.mySystemMenuBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(113)))), ((int)(((byte)(206)))));
+            this.mySystemMenuBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.mySystemMenuBtn.FlatAppearance.BorderSize = 0;
+            this.mySystemMenuBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mySystemMenuBtn.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mySystemMenuBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.mySystemMenuBtn.Location = new System.Drawing.Point(0, 184);
+            this.mySystemMenuBtn.Name = "mySystemMenuBtn";
+            this.mySystemMenuBtn.Size = new System.Drawing.Size(260, 92);
+            this.mySystemMenuBtn.TabIndex = 6;
+            this.mySystemMenuBtn.Text = "My System";
+            this.mySystemMenuBtn.UseVisualStyleBackColor = false;
+            this.mySystemMenuBtn.Click += new System.EventHandler(this.mySystemMenuBtn_Click);
+            this.mySystemMenuBtn.MouseEnter += new System.EventHandler(this.mySystemMenuBtn_MouseEnter);
+            this.mySystemMenuBtn.MouseLeave += new System.EventHandler(this.mySystemMenuBtn_MouseLeave);
+            // 
+            // employeeMenuBtn
+            // 
+            this.employeeMenuBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(113)))), ((int)(((byte)(206)))));
+            this.employeeMenuBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.employeeMenuBtn.FlatAppearance.BorderSize = 0;
+            this.employeeMenuBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.employeeMenuBtn.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeMenuBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.employeeMenuBtn.Location = new System.Drawing.Point(0, 92);
+            this.employeeMenuBtn.Name = "employeeMenuBtn";
+            this.employeeMenuBtn.Size = new System.Drawing.Size(260, 92);
+            this.employeeMenuBtn.TabIndex = 5;
+            this.employeeMenuBtn.Text = "Employees";
+            this.employeeMenuBtn.UseVisualStyleBackColor = false;
+            this.employeeMenuBtn.Click += new System.EventHandler(this.employeeMenuBtn_Click);
+            this.employeeMenuBtn.MouseEnter += new System.EventHandler(this.employeeMenuBtn_MouseEnter);
+            this.employeeMenuBtn.MouseLeave += new System.EventHandler(this.employeeMenuBtn_MouseLeave);
+            // 
+            // assetMenuBtn
+            // 
+            this.assetMenuBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(113)))), ((int)(((byte)(206)))));
+            this.assetMenuBtn.Dock = System.Windows.Forms.DockStyle.Top;
+            this.assetMenuBtn.FlatAppearance.BorderSize = 0;
+            this.assetMenuBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.assetMenuBtn.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.assetMenuBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.assetMenuBtn.Location = new System.Drawing.Point(0, 0);
+            this.assetMenuBtn.Name = "assetMenuBtn";
+            this.assetMenuBtn.Size = new System.Drawing.Size(260, 92);
+            this.assetMenuBtn.TabIndex = 4;
+            this.assetMenuBtn.Text = "Assets";
+            this.assetMenuBtn.UseVisualStyleBackColor = false;
+            this.assetMenuBtn.Click += new System.EventHandler(this.assetMenuBtn_Click);
+            this.assetMenuBtn.MouseEnter += new System.EventHandler(this.assetMenuBtn_MouseEnter);
+            this.assetMenuBtn.MouseLeave += new System.EventHandler(this.assetMenuBtn_MouseLeave);
+            // 
+            // addAssetBtn
+            // 
+            this.addAssetBtn.Location = new System.Drawing.Point(640, 424);
+            this.addAssetBtn.Name = "addAssetBtn";
+            this.addAssetBtn.Size = new System.Drawing.Size(228, 73);
+            this.addAssetBtn.TabIndex = 5;
+            this.addAssetBtn.Text = "Add Asset";
+            this.addAssetBtn.UseVisualStyleBackColor = true;
+            this.addAssetBtn.Click += new System.EventHandler(this.addAssetBtn_Click);
+            // 
+            // listViewAssets
+            // 
+            this.listViewAssets.HideSelection = false;
+            this.listViewAssets.Location = new System.Drawing.Point(285, 92);
+            this.listViewAssets.Name = "listViewAssets";
+            this.listViewAssets.Size = new System.Drawing.Size(957, 326);
+            this.listViewAssets.TabIndex = 4;
+            this.listViewAssets.UseCompatibleStateImageBehavior = false;
+            this.listViewAssets.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // LandingScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1269, 822);
-            this.Controls.Add(this.loggedAssestsLabel);
-            this.Controls.Add(this.landingScreenP2);
-            this.Controls.Add(this.landingScreenP1);
+            this.ClientSize = new System.Drawing.Size(1269, 935);
+            this.Controls.Add(this.addAssetBtn);
+            this.Controls.Add(this.listViewAssets);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.headingLbl);
             this.Name = "LandingScreen";
             this.Text = "LandingScreen";
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel landingScreenP1;
-        private System.Windows.Forms.Panel landingScreenP2;
-        private System.Windows.Forms.Label loggedAssestsLabel;
+        private System.Windows.Forms.Label headingLbl;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button mySystemMenuBtn;
+        private System.Windows.Forms.Button employeeMenuBtn;
+        private System.Windows.Forms.Button assetMenuBtn;
+        private System.Windows.Forms.Button addAssetBtn;
+        private System.Windows.Forms.ListView listViewAssets;
     }
 }
--- LandingScreen.cs
+++ LandingScreen.cs
@@ -15,6 +15,93 @@ namespace ScottishGlen
         public LandingScreen()
         {
             InitializeComponent();
+            listViewAssets.View = View.Details;
+            listViewAssets.FullRowSelect = true;
+            listViewAssets.GridLines = true;
+            listViewAssets.Columns.Clear();
+            listViewAssets.Columns.Add("ID", 50);
+            listViewAssets.Columns.Add("Name", 150);
+            listViewAssets.Columns.Add("Purchase Date", 100);
+            listViewAssets.Columns.Add("Employee Id", 100);
+            listViewAssets.Columns.Add("Notes", 100);
+        }
+
+        private void assetMenuBtn_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private void assetMenuBtn_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+
+        private void employeeMenuBtn_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private void employeeMenuBtn_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+
+        private void mySystemMenuBtn_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private void mySystemMenuBtn_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addAssetBtn_Click(object sender, EventArgs e)
+        {
+            AddAsset addAssetForm = new AddAsset();
+            addAssetForm.Show();
+        }
+
+        private void assetMenuBtn_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void employeeMenuBtn_Click(object sender, EventArgs e)
+        {   
+            headingLbl.Text = "Employees";
+            listViewAssets.View = View.Details;
+            listViewAssets.FullRowSelect = true;
+            listViewAssets.GridLines = true;
+            listViewAssets.Columns.Clear();
+            listViewAssets.Columns.Add("ID", 50);
+            listViewAssets.Columns.Add("First Name", 150);
+            listViewAssets.Columns.Add("Last Name", 100);
+            listViewAssets.Columns.Add("Email Address", 100);
+            listViewAssets.Columns.Add("Department Id", 100);
+        }
+
+        private void mySystemMenuBtn_Click(object sender, EventArgs e)
+        {   
+            headingLbl.Text = "My System";
+            listViewAssets.View = View.Details;
+            listViewAssets.FullRowSelect = true;
+            listViewAssets.GridLines = true;
+            listViewAssets.Columns.Clear();
+            listViewAssets.Columns.Add("System ID", 50);
+            listViewAssets.Columns.Add("System Name", 50);
+            listViewAssets.Columns.Add("Model", 50);
+            listViewAssets.Columns.Add("Manufacturer", 50);
+            listViewAssets.Columns.Add("System Type", 50);
+            listViewAssets.Columns.Add("IP Address", 50);
+            listViewAssets.Columns.Add("Date Captured", 50);
+            listViewAssets.Columns.Add("Employee Id", 50);
         }
     }
 }
