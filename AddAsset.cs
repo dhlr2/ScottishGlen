75751fb450659ceace13bb14e7a04affaca45f37
Darren <2504497@uad.ac.uk>
2025-11-04 12:53:31 +0000

    Added summary

--- AddAsset.cs
+++ AddAsset.cs
@@ -17,6 +17,14 @@ namespace ScottishGlen
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Handles the click event for the confirm asset button, creating a new asset and adding it to the database.
+        /// </summary>
+        /// <remarks>This method retrieves asset details from the user interface, constructs an <see
+        /// cref="Asset"/> object, and invokes the method to add the asset to the database. Ensure that the employee ID
+        /// is a valid integer.</remarks>
+        /// <param name="sender">The source of the event, typically the confirm asset button.</param>
+        /// <param name="e">The event data associated with the click event.</param>
         private void confirmAssetBtn_Click(object sender, EventArgs e)
         {
             Asset asset = new Asset
